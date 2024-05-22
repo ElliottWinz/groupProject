ec558e1da111c02651fd78d9023d3b5ae03d2bb6
ElliottWinz <elliottwinz@outlook.com>
2024-05-22 15:07:23 -0700

    attempt

--- Group Project/Assets/Scripts/PlayerController.cs
+++ Group Project/Assets/Scripts/PlayerController.cs
@@ -7,23 +7,43 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject player;
     public GameObject root;
+    public GameObject recoiler;
+    public bool follow;
+    public Vector3 direction;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        follow = true;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        direction = (player.transform.position - root.transform.position);        
 
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 direction = (root.transform.position - player.transform.position);
-            player.GetComponent<Rigidbody>().AddForce(direction * 100f);
+            Recoil(true, direction);
         }
     }
+
+    void Recoil(bool recoil, Vector3 direction)
+    {
+        if (recoil)
+        {
+            recoiler.GetComponent<Rigidbody>().AddForce(direction * 100f);
+            follow = true;
+        }
+        else
+        {
+            follow = false;
+        }
+    }
+
 
 }
