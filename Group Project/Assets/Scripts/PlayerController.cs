68a6ebbbc1dc093b14fdb9d834ed8159ef11069d
ElliottWinz <elliottwinz@outlook.com>
2024-05-23 15:10:26 -0700

    trying to move over

--- Group Project/Assets/Scripts/PlayerController.cs
+++ Group Project/Assets/Scripts/PlayerController.cs
@@ -3,27 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
     public GameObject player;
     public GameObject root;
+    public bool follow;
+    public Vector3 direction;
+    private CharacterController control;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+        follow = true;
+        control = GetComponent<CharacterController>();
+       
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        direction = (player.transform.position - root.transform.position);        
+
 
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 direction = (root.transform.position - player.transform.position);
-            player.GetComponent<Rigidbody>().AddForce(direction * 100f);
+            Vector3 direction = (player.transform.position - root.transform.position);
+            Recoil(true, direction);
         }
     }
 
+
+    void Recoil(bool recoil, Vector3 direction)
+    {
+        if (recoil)
+        {
+            control.Move(direction.normalized * 5f + new Vector3(0.0f, (direction.y + 1f) * 50f, 0.0f) * Time.deltaTime);
+            follow = true;
+        }
+        else
+        {
+            follow = false;
+        }
+    }
+
+
+
+
 }
+
+
